2d55fd3072587c9c648099bb5bad0d63401bc5c6
maple-r <ttlle11@naver.com>
2020-06-15 18:08:27 +0900

    107

--- docs/common/js/common.js
+++ docs/common/js/common.js
@@ -1,21 +1,6 @@
 var version = '334';
 var region = 'KMS';
 
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
-	dropdown[i].addEventListener("click", function() {
-		this.classList.toggle("active");
-		var dropdownContent = this.nextElementSibling;
-		if (dropdownContent.style.display === "block") {
-			dropdownContent.style.display = "none";
-		} else {
-			dropdownContent.style.display = "block";
-		}
-	});
-}
-
 $(document).ready(function(){
 
 	document.addEventListener("DOMContentLoaded", function() {
