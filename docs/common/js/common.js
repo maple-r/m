5e8611528c217c57c6bd74bd40924b2bbe86ae63
maple-r <ttlle11@naver.com>
2020-06-15 17:17:57 +0900

    88

--- docs/common/js/common.js
+++ docs/common/js/common.js
@@ -212,6 +212,12 @@ function fnCngSkin(){
 }
 
 function fnCngAni(){
+	var ani = document.getElementById("ani").value;
+  if(ani != 'default'){
+    FaceArr = ',{"itemId":' + id + ',"animationName":"'+ ani +'","version":"' + version + '","region":"' + region + '"}';
+  }else{
+    FaceArr = ',{"itemId":' + id + ',"version":"' + version + '","region":"' + region + '"}';
+  }
 	fnSetSrc();
 }
 
