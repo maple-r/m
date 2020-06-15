5c5b84897ec05a0f2a569b8b238ba4e4b8d2eec7
maple-r <ttlle11@naver.com>
2020-06-15 18:03:00 +0900

    104

--- docs/common/js/itemShow/faceshow.js
+++ docs/common/js/itemShow/faceshow.js
@@ -1,13 +1,29 @@
+$(document).ready(function(){
+	var dropdown = document.getElementsByClassName("dropdown-btn");
+	var i;
+
+	for (i = 0; i < dropdown.length; i++) {
+		dropdown[i].addEventListener("click", function() {
+			this.classList.toggle("active");
+			var dropdownContent = this.nextElementSibling;
+			if (dropdownContent.style.display === "block") {
+				dropdownContent.style.display = "none";
+			} else {
+				dropdownContent.style.display = "block";
+			}
+		});
+	}
+});
 
 function fnFaceShow(){
-	$('#Fbtn').toggleClass("active");
-	var dropdownContent = document.getElementById('Fbtn').nextElementSibling; 
+	/*$('#Fbtn').toggleClass("active");
+	var dropdownContent = document.getElementById('Fbtn').nextElementSibling;
 	console.log(dropdownContent.style.display);
 	if (dropdownContent.style.display === "block") {
 		dropdownContent.style.display = "none";
 	} else {
 		dropdownContent.style.display = "block";
-	}
+	}*/
 
 	$('#Fbtn').prop("onclick", null).attr("onclick", null)
 	var gender = $("input[name='isCondi']:checked").val();
