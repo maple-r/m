de001ddde3eaa17ac5f284f95f35ba56e12ff749
maple-r <ttlle11@naver.com>
2020-06-16 11:57:32 +0900

    142

--- docs/common/js/srchbar.js
+++ docs/common/js/srchbar.js
@@ -15,6 +15,7 @@ function fnSrch(){
 				isCondi:val2
 			},
 			success: function (response){
+				console.log($(response).find('#mySidenav'));
 				var rt = response.replace('<div id="mySidenav" class="sidenav">', '').replace('<!--눈장 메뉴 end-->\n</div>', '');
 				$("#mySidenav").html(rt);
 
